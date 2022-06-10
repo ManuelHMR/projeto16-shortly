2555f3820c66f88fe35f7b4a1d9031222f79797c
ManuelHMR <manuelhmrjf@gmail.com>
2022-06-10 15:23:46 -0300

    fix timestamp at tables

--- tables.sql
+++ tables.sql
@@ -2,7 +2,8 @@ CREATE TABLE "users" (
     "id" SERIAL PRIMARY KEY,
     "username" TEXT NOT NULL,
     "email" TEXT UNIQUE NOT NULL,
-    "password" TEXT NOT NULL
+    "password" TEXT NOT NULL,
+    "createdAt"  TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT NOW()  
 );
 
 CREATE TABLE "urls" (
@@ -10,5 +11,6 @@ CREATE TABLE "urls" (
     "url" TEXT NOT NULL,
     "shortUrl" TEXT UNIQUE NOT NULL,
     "visits" INTEGER NOT NULL DEFAULT(0),
-    "userId" INTEGER REFERENCES users(id)
+    "userId" INTEGER REFERENCES users(id),
+    "createdAt"  TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT NOW()
 );
