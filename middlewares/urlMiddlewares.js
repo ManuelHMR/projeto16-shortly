e9c42278456e03e092c6afef01deb10942ab679d
ManuelHMR <manuelhmrjf@gmail.com>
2022-06-10 11:45:26 -0300

    feat-url-middleware

--- middlewares/urlMiddlewares.js
+++ middlewares/urlMiddlewares.js
@@ -1,1 +1,14 @@
 import joi from "joi";
+
+const urlSchema = joi.object({
+    url: joi.string().required()
+});
+
+export async function urlValidation (req, res, next){
+    const validation = urlSchema.validate(req.body);
+    if(validation.error){
+        const errorArr = validation.error.details;
+        return res.status(422).send(errorArr.map((e) => {return e.message}));
+    };
+    next();
+};
--- routers/urlRouters.js
+++ routers/urlRouters.js
@@ -2,10 +2,11 @@ import express from "express";
 
 import { postUrl } from "../controllers/urlControllers.js";
 import { authMiddlewares } from "../middlewares/authMiddleware.js";
+import { urlValidation } from "./../middlewares/urlMiddlewares.js"
 
 const urlRouters = express.Router();
 
-urlRouters.post("/urls/shorten", authMiddlewares, postUrl);
+urlRouters.post("/urls/shorten", authMiddlewares, urlValidation, postUrl);
 // urlRouters.get();
 // urlRouters.get();
 // urlRouters.delete();
